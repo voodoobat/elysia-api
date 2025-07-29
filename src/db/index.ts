897d9e2e0476d2837947e7b2f8595e29dda26aa3
Vova Rodin <mynameisvova@yahoo.com>
2025-07-29 12:47:45 +0300

    feat: adds prisma

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from './generated'
+
+const prismaClientSingleton = () => new PrismaClient()
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+export const db = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (Bun.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = db
+}
